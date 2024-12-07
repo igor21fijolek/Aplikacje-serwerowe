3ac990bf67b29c668f4a9e0220c4140935d95a4e
igor21fijolek <fijolekigor7@gmail.com>
2024-12-07 19:26:23 +0100

    na kartkowke w czwartek
    na kartkowke w czwartek

--- 5.12/client/app.js
+++ 5.12/client/app.js
@@ -17,16 +17,65 @@ let p = document.getElementById("wynik")
 
 
 
-async function palindrom(){
+async function palindrom(){ 
     let word = document.getElementById("zad2").value
-    const url = `http://localhost:3000/palindrome/${word}`
-    let data =  await fetch(url)
-   const json = await data.json()
-   let tab = [word]
+    let data =  await fetch(`http://localhost:3000/palindrome/${word}`)
+    const wynik = await data.json()
+    document.getElementById("wynik2").innerHTML = wynik
+}
 
-   if(word == tab.reverse()){
-    document.getElementById("wynik2").textContent = 'podany wyraz jest palindromem'
-   }else{
-    document.getElementById("wynik2").textContent = 'podany wyraz nie jest palindromem'
-   }
+async function duzeLitery(){
+    let text = document.getElementById("zad3").value
+    let data =  await fetch(`http://localhost:3000/uppercase/${text}`)
+    const wynik = await data.json()
+    document.getElementById("wynik3").innerHTML = wynik
+}
+
+async function losujLiczbe(){
+    let min = document.getElementById("l1").value
+    let max = document.getElementById("l2").value
+    let data =  await fetch(`http://localhost:3000/random/${min}/${max}`)
+    const wynik = await data.json()
+    document.getElementById("wynik4").innerHTML = wynik
+}
+
+async function odwrotnyWyraz(){
+    let text = document.getElementById("zad5").value
+    let data =  await fetch(`http://localhost:3000/reverse/${text}`)
+    const wynik = await data.json()
+    document.getElementById("wynik5").innerHTML = wynik
+}
+
+async function length() {
+    let text = document.getElementById("zad6").value
+    let data =  await fetch(`http://localhost:3000/dlugosc/${text}`)
+    const wynik = await data.json()
+    document.getElementById("wynik6").innerHTML = wynik
+}
+
+async function fibonacci() {
+    let num = document.getElementById("zad7").value
+    let data =  await fetch(`http://localhost:3000/fibonacci/${num}`)
+    const wynik = await data.json()
+    document.getElementById("wynik7").innerHTML = wynik
+}
+
+async function przelicz() {
+    let grade = document.getElementById("zad8").value
+    let data =  await fetch(`http://localhost:3000/przelicz/${grade}`)
+    const wynik = await data.json()
+    document.getElementById("wynik8").innerHTML = wynik    
+}
+async function zdanie() {
+    let sentence = document.getElementById("zad9").value
+    let data =  await fetch(`http://localhost:3000/reverseWords/${sentence}`)
+    const wynik = await data.json()
+    document.getElementById("wynik9").innerHTML = wynik  
+}
+async function czyEven() {
+    let num = document.getElementById("zad10").value
+    let data =  await fetch(`http://localhost:3000/even/${num}`)
+    const wynik = await data.json()
+    document.getElementById("wynik10").innerHTML = wynik  
+    
 }
--- 5.12/serwer/index.js
+++ 5.12/serwer/index.js
@@ -14,9 +14,78 @@ app.get("/suma/:liczba1/:liczba2", (req, res)=>{
 
 app.get("/palindrome/:word", (req,res)=>{
     let word = req.params.word
+    let reversedWord = word.split('').reverse().join('');
+    if(word == reversedWord){
+        res.json(`słowo ${word} jest palindromem`)
+    }else{
+        res.json(`słowo ${word} nie jest palindromem`)
+    }
+})
+
+app.get("/uppercase/:text", (req,res)=>{
+    let text = req.params.text
+    upperText = text.toUpperCase()
+    if(text != upperText){
+        res.json(`wyraz przekształcony na duze litery to: ${upperText}`)
+    }else if(text == upperText){
+        res.json(`wyraz przekształcony na duze litery to: ${upperText}`)
+    }else{
+        res.json(`wyraz przekształcony na duze litery to: ${upperText}`)
+    }
+})
+
+app.get("/random/:min/:max", (req,res)=>{
+    let min = parseInt(req.params.min)
+    let max = parseInt(req.params.max)
+    let random = Math.floor(Math.random() * (max - min + 1) + min)
+    res.json(`losowa liczba z przedziału od ${min} do ${max} to: ${random}`)
+})
+
+app.get("/reverse/:text", (req,res)=>{
+    let text = req.params.text
+    let reverse = text.split('').reverse().join('');
+    res.json(`odwrocony tekst to: ${reverse}`)
+})
+
+app.get("/fibonacci/:num", (req,res)=>{
+    let num = parseInt(req.params.num)
+    let fib = [0,1]
+    for(let i=2; i < num; i++){
+        fib.push(fib[i-1] + fib[i-2])
+    }
+    res.json(`ciag fibonacciego to dlugosci ${num} to: ${fib}`)
+})
+
+app.get("/dlugosc/:text", (req,res)=>{
+    let text = req.params.text
+    let text2 = text.length
+    res.json(`długośc podanego tekstu to: ${text2}`)
 })
 
 
+app.get("/przelicz/:grade", (req,res)=>{
+    let grade = req.params.grade
+    let farenheit = grade * 1.8 + 32
+    res.json(`${grade} stopni C to: ${farenheit} stopni F`)
+})
+
+
+app.get("/reverseWords/sentence", (req,res)=>{
+    let sentence = req.params.sentence
+
+    let rev = sentence.split(" ").reverse().join(" ")
+    res.json(`odwrocone zdanie to: ${rev}`)
+})
+
+app.get("/even/:num", (req,res)=>{
+    let num  = parseInt(req.params.num)
+    if(num % 2 == 0){
+        res.json(`liczba ${num} jest parzysta`)
+    }else{
+        res.json(`liczba ${num} nie jest parzysta`)
+    }
+})
+
 app.listen(3000,()=>{
-    console.log('aplikacja działa');
+    console.log('aplikacja działa'); 
 })
